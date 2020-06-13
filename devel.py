483d0718596496745d14727596b5981ebd6a7f84
Loc Lam <44765059+kwantonomics@users.noreply.github.com>
2020-06-13 16:22:10 +0700

    update place_stoploss function

--- devel.py
+++ devel.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support.ui import WebDriverWait
 from threading import Thread
 from time import sleep
@@ -83,35 +84,43 @@ class FuturesTrade(webdriver.Chrome):
         return result_table
     
 
-    def entry_order(self, entry_strategies, stoploss=True, wait=10):
+    def entry_order(self, entry_strategies, wait=10):
         self.entry_strategies = entry_strategies
+        stoploss_strategies = self._set_exit_strategies()
         
-        for contract in entry_strategies:
+        for contract in self.entry_strategies:
             try:
                 self.place_single_order(
                     contract,
-                    entry_strategies[contract]['direction'],
-                    entry_strategies[contract]['volume']
+                    self.entry_strategies[contract]['direction'],
+                    self.entry_strategies[contract]['volume'],
+                    self.entry_strategies[contract]['tolerance']
                 )
                 
                 # Verify client
                 self._verify_client()
 
+                # Update status
+                self.entry_strategies[contract]['status'] = self.check_status(
+                    contract,
+                    self.entry_strategies[contract]['direction']
+                )
+
             except Exception as e:
-                self.entry_strategies[contract]['entry_status'] = 'Failed'
+                self.entry_strategies[contract]['status'] = 'Failed'
                 print(f'{self.client["name"]} - {self.client["username"]} - '
                       f'Could not place {entry_strategies[contract]["direction"]} order for {contract}. '
                       f'Error message: {e}')
                 pass
             
-            if stoploss:
+            if stoploss_strategies['order_stoploss'] and stoploss_strategies['trigger_stoploss']:
                 try:
                     self.place_single_stoploss(
                         contract,
-                        self.entry_strategies[contract]['direction'],
-                        self.entry_strategies[contract]['volume'],
-                        self.entry_strategies[contract]['stoploss1'],
-                        self.entry_strategies[contract]['stoploss2']
+                        stoploss_strategies[contract]['direction'],
+                        stoploss_strategies[contract]['volume'],
+                        stoploss_strategies[contract]['order_stoploss'],
+                        stoploss_strategies[contract]['trigger_stoploss']
                     )
                 except Exception as e:
                     self.entry_strategies[contract]['stoploss_status'] = 'Failed'
@@ -127,17 +136,15 @@ class FuturesTrade(webdriver.Chrome):
     
 
     def exit_order(self, exit_strategies):
-        for contract in exit_strategies:
-            if self.entry_strategies[contract]['direction'] == 'long':
-                exit_strategies[contract]['direction'] = 'short'
-            elif self.entry_strategies[contract]['direction'] == 'short':
-                exit_strategies[contract]['direction'] = 'long'
-            
+        exit_strategies = self._set_exit_strategies(exit_strategies)
+        
+        for contract in exit_strategies:            
             try:
                 self.place_single_order(
                     contract,
                     exit_strategies[contract]['direction'],
-                    exit_strategies[contract]['volume']
+                    exit_strategies[contract]['volume'],
+                    exit_strategies[contract]['tolerance']
                 )
             except Exception as e:
                 print(f'{self.client["name"]} - {self.client["username"]} - '
@@ -150,7 +157,8 @@ class FuturesTrade(webdriver.Chrome):
 
     def cancel_order(self):
         for contract in self.entry_strategies:
-            self.cancel_single_order(contract)
+            direction = self.entry_strategies['direction']
+            self.cancel_single_order(contract, direction)
             print(f'{self.client["name"]} - {self.client["username"]} - '
                   f'Entry order for contract {contract} is canceled '
                   f'because of being unable to be filled.')
@@ -158,20 +166,28 @@ class FuturesTrade(webdriver.Chrome):
         return
 
     
-    def place_stoploss(self, stoploss_strategies=None):
-        if not stoploss_strategies:
-            stoploss_strategies = self.entry_strategies
+    def place_stoploss(self, stoploss_strategies={}):
+        stoploss_strategies = self._set_exit_strategies(stoploss_strategies)
         
         for contract in stoploss_strategies:
             try:
+                # Cancel previous stoploss order if any
+                try:
+                    self.cancel_single_order(contract, stoploss_strategies[contract]['direction'])
+                except:
+                    pass
+                
+                # Place stoploss order
                 self.place_single_stoploss(
                     contract,
                     stoploss_strategies[contract]['direction'],
                     stoploss_strategies[contract]['volume'],
-                    stoploss_strategies[contract]['stoploss1'],
-                    stoploss_strategies[contract]['stoploss2']
+                    stoploss_strategies[contract]['order_stoploss'],
+                    stoploss_strategies[contract]['trigger_stoploss']
                 )
+            
             except Exception as e:
+                self.entry_strategies[contract]['stoploss_status'] = 'Failed'
                 print(f'{self.client["name"]} - {self.client["username"]} - '
                       f'Could not place stop loss order for {contract}. Error message: {e}')
                 continue
@@ -179,51 +195,42 @@ class FuturesTrade(webdriver.Chrome):
         return
 
 
-    def place_single_order(self, contract, direction, volume):
+    def place_single_order(self, contract, direction, volume, tolerance=0.3):
         self.refresh()
         
-        unlock_button = self.find_element_by_id(f'locked_{contract}')
-        volume_input = self.find_element_by_id(f'vol_{contract}')
-        long_order = self.find_element_by_xpath(f'//*[@id="srow_{contract}"]/table/tbody/tr[2]/td[1]')
-        short_order = self.find_element_by_xpath(f'//*[@id="srow_{contract}"]/table/tbody/tr[2]/td[4]')
-
         # Unlock button
-        try:
-            unlock_button.click()
-        except:
-            pass
+        self._unlock_button(contract)
         
         # Input volume
-        volume_input.clear()
-        volume_input.send_keys(str(volume))
+        self._input_volume(contract, volume)
+
+        # Set tolerance
+        self._set_tolerance(contract, tolerance)
 
         # Place order
-        if direction == 'long':
-            long_order.click()
-        elif direction == 'short':
-            short_order.click()
-        else:
-            raise ValueError(f'{self.client["name"]} - {self.client["username"]} - '
-                             'Direction must be either long or short')
+        self._proceed_order(contract, direction)
         
+        # Select market order type -- Incomplete development
+        # self._select_market_order_type(market_order_type)
+
         sleep(0.5)
         
-        return self.check_status(contract)
+        return self.check_status(contract, direction)
 
     
-    def place_single_stoploss(self, contract, direction, volume, stoploss1, stoploss2):
+    def place_single_stoploss(self, contract, direction, volume, order_stoploss, trigger_stoploss):
         self.refresh()
 
         # Check status
-        status = self.check_status(contract)
+        status = self.check_status(contract, direction)
         
         if status == 'Matched':
 
             # Check which type of stop loss to go with
-            if direction == 'long':
+            if direction == 'short':
                 stoploss_order = self.find_element_by_id('sTab')
                 action_button = self.find_element_by_id('enterSell')
-            elif direction == 'short':
+            elif direction == 'long':
                 stoploss_order = self.find_element_by_id('bTab')
                 action_button = self.find_element_by_id('enterBuy')
             else:
@@ -236,21 +243,21 @@ class FuturesTrade(webdriver.Chrome):
             condition_box = self.find_element_by_id('bOrderConChk')
             stoploss_contract = self.find_element_by_id('bStockID')
             volume_input = self.find_element_by_id('bVolume')
-            stoploss_1_input = self.find_element_by_id('bPrice')
-            stoploss_2_input = self.find_element_by_id('bPriceCon')
+            order_stoploss_input = self.find_element_by_id('bPrice')
+            trigger_stoploss_input = self.find_element_by_id('bPriceCon')
 
             if not condition_box.is_selected():
                 condition_box.click()
             
             stoploss_contract.clear()
             volume_input.clear()
-            stoploss_1_input.clear()
-            stoploss_2_input.clear()
+            order_stoploss_input.clear()
+            trigger_stoploss_input.clear()
 
             stoploss_contract.send_keys(str(contract))
             volume_input.send_keys(str(volume))
-            stoploss_1_input.send_keys(str(stoploss1))
-            stoploss_2_input.send_keys(str(stoploss2))
+            order_stoploss_input.send_keys(str(order_stoploss))
+            trigger_stoploss_input.send_keys(str(trigger_stoploss))
 
             action_button.click()
         
@@ -262,13 +269,12 @@ class FuturesTrade(webdriver.Chrome):
         return
     
 
-    def cancel_single_order(self, contract):
-        status = self.check_status(contract)
-        self.entry_strategies[contract]['entry_status'] = status
+    def cancel_single_order(self, contract, direction):
+        status = self.check_status(contract, direction)
         
-        if self.entry_strategies[contract]['entry_status'] == 'Pending':
+        if status == 'Pending':
             status_table = self._get_status_table()
-            contract_index = self._get_contract_index(status_table, contract)
+            contract_index = self._get_contract_index(status_table, contract, direction)
             contract_index += 1
             button_xpath = f'//*[@id="grdTodayHis"]/tr[{contract_index}]/td[3]/button'
             cancel_button = self.find_element_by_xpath(button_xpath)
@@ -282,13 +288,13 @@ class FuturesTrade(webdriver.Chrome):
         return
     
 
-    def check_status(self, contract):
+    def check_status(self, contract, direction):
         self.refresh()
         sleep(0.1)
 
         try:
             status_table = self._get_status_table()
-            contract_index = self._get_contract_index(status_table, contract)
+            contract_index = self._get_contract_index(status_table, contract, direction)
             status = status_table.iloc[contract_index, 13]
             status = status.replace(' ', '')
 
@@ -318,10 +324,12 @@ class FuturesTrade(webdriver.Chrome):
         return
     
 
-    def _get_contract_index(self, status_table, contract):
+    def _get_contract_index(self, status_table, contract, direction):
         try:
-            contract_verify = status_table.iloc[:, 6].str.replace(' ', '') == contract
-            contract_index = contract_verify[contract_verify == True].index[0]
+            contract_verification = status_table.iloc[:, 6].str.replace(' ', '') == contract
+            direction_verification = status_table.iloc[:, 7].str.replace(' ', '') == direction
+            final_verification = contract_verification & direction_verification
+            contract_index = final_verification[final_verification is True].index[0]
         except:
             msg = f'Could not find contract {contract} in list of current trades'
             raise ValueError(msg)
@@ -343,6 +351,71 @@ class FuturesTrade(webdriver.Chrome):
             else:
                 raise ValueError(e)
     
+
+    # def _select_market_order_type(self, market_order_type): # Incomplete development
+    #     select = Select(self.find_element_by_id('bType'))
+    #     select.select_by_visible_text(market_order_type)
+
+    #     return
+    
+
+    def _proceed_order(self, contract, direction):
+        if direction == 'long':
+            execution_button = self.find_element_by_xpath(f'//*[@id="srow_{contract}"]/table/tbody/tr[2]/td[1]')
+        elif direction == 'short':
+            execution_button = self.find_element_by_xpath(f'//*[@id="srow_{contract}"]/table/tbody/tr[2]/td[4]')
+        else:
+            raise ValueError(f'{self.client["name"]} - {self.client["username"]} - '
+                             'Direction must be either long or short')
+        
+        execution_button.click()
+
+        return
+    
+
+    def _set_exit_strategies(self, exit_strategies={}):
+        if not exit_strategies:
+            exit_strategies = self.entry_strategies.copy()
+        
+        for contract in exit_strategies:
+            # Identify direction of the stoploss order
+            if self.entry_strategies[contract]['direction'] == 'long':
+                exit_strategies[contract]['direction'] = 'short'
+            elif self.entry_strategies[contract]['direction'] == 'short':
+                exit_strategies[contract]['direction'] = 'long'
+            
+            # Adopt volume from entry_strategies
+            if 'volume' not in exit_strategies[contract]:
+                exit_strategies[contract]['volume'] = self.entry_strategies[contract]['volume'].copy()
+        
+        return exit_strategies
+    
+
+    def _set_tolerance(self, contract, tolerance):
+        slippage = self.find_element_by_xpath(f'//*[@id="ampli_{contract}"]')
+        slippage.clear()
+        slippage.send_keys(str(tolerance))
+
+        return
+    
+
+    def _input_volume(self, contract, volume):
+        volume_input = self.find_element_by_id(f'vol_{contract}')
+        volume_input.clear()
+        volume_input.send_keys(str(volume))
+
+        return
+    
+
+    def _unlock_button(self, contract):
+        unlock_button = self.find_element_by_id(f'locked_{contract}')
+        try:
+            unlock_button.click()
+        except:
+            pass
+
+        return
+
 
     def _verify_client(self):
         confirm_button = WebDriverWait(self, 5).until(
@@ -414,7 +487,7 @@ class MultiAccTrade():
         return
     
     
-    def place_stoploss(self, stoploss_strategies=None):
+    def place_stoploss(self, stoploss_strategies={}):
         self._run_multithreads('stoploss', stoploss_strategies)
         
         return
